15793ae14c637c2781e8d7fa5dd599a8b7dd1091
GiridharAparajith <giridhar05072004@gmail.com>
2024-10-27 17:56:37 +0530

    feat:added speak

---
+++ javrivs.py
@@ -0,0 +1,27 @@
+import pyttsx3 #pip install pyttsx3
+import datetime
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = datetime.datetime.now().hour
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+    speak("I am Jarvis Sir. Please tell me how may I help you")       
+
+        
+if __name__ == "_main_":
+    wishMe()
